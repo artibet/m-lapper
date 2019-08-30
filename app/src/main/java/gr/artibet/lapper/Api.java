87524cadd7240a28620bd5df9a4633fe0498656d
Tsakiridis Sotiris <sotiris@serres.gr>
2019-08-30 14:32:36 +0300

    Install retrofit/gson

---
+++ app/src/main/java/gr/artibet/lapper/Api.java
@@ -0,0 +1,7 @@
+package gr.artibet.lapper;
+
+public interface Api {
+
+    @POST("/api/auth-token/")
+
+}
--- app/src/main/java/gr/artibet/lapper/LoginActivity.java
+++ app/src/main/java/gr/artibet/lapper/LoginActivity.java
@@ -3,23 +3,61 @@ package gr.artibet.lapper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText mTextUsername;
-    EditText mTextpassword;
-    Button mButtonLogin;
+    EditText mEditTextUsername;
+    EditText mEditTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mTextUsername = findViewById(R.id.edittext_username);
-        mTextpassword = findViewById(R.id.edittext_password);
-        mButtonLogin = findViewById(R.id.button_login);
+        mEditTextUsername = findViewById(R.id.editTextUsername);
+        mEditTextPassword = findViewById(R.id.editTextPassword);
+
+        // Set click listener for login button
+        findViewById(R.id.buttonLogin).setOnClickListener(this);
 
     }
+
+    // Login method
+    public void login() {
+        String username = mEditTextUsername.getText().toString().trim();
+        String password = mEditTextPassword.getText().toString().trim();
+
+        // username validation
+        if (username.isEmpty()) {
+            mEditTextUsername.setError(getString(R.string.username_required));
+            mEditTextUsername.requestFocus();
+            return;
+        }
+
+        // password validation
+        if (password.isEmpty()) {
+            mEditTextPassword.setError(getString(R.string.password_required));
+            mEditTextPassword.requestFocus();
+            return;
+        }
+
+        // Do user login through api
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.buttonLogin:
+                login();
+                break;
+        };
+
+    }
+
+
 }

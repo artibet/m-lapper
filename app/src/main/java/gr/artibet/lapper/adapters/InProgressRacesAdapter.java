d955f6c031b8c816f239b735852882fd45a289eb
Tsakiridis Sotiris <sotiris@serres.gr>
2019-10-06 10:36:30 +0300

    Create inprogress races recycler view

---
+++ app/src/main/java/gr/artibet/lapper/adapters/InProgressRacesAdapter.java
@@ -0,0 +1,187 @@
+package gr.artibet.lapper.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import gr.artibet.lapper.R;
+import gr.artibet.lapper.Util;
+import gr.artibet.lapper.models.Race;
+
+public class InProgressRacesAdapter extends RecyclerView.Adapter<InProgressRacesAdapter.InProgressRacesViewHolder> {
+
+    // Context
+    private static Context mContext;
+
+    // Vehicle List
+    private List<Race> mRaceList;
+
+    // item click listener member
+    private OnItemClickListener mItemListener;
+
+
+    // Item click interface
+    public interface OnItemClickListener {
+        void onCancel(int position);
+        void onViewVehicles(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mItemListener = listener;
+    }
+
+
+    // VIEW HOLDER CLASS
+    public static class InProgressRacesViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView ivPublic;
+        public ImageView ivMenu;
+
+        public TextView tvTag;
+        public TextView tvCreator;
+        public TextView tvStartMethod;
+        public TextView tvMode;
+        public TextView tvVehiclesNo;
+        public TextView tvLaps;
+        public TextView tvInProgress;
+        public TextView tvCanceled;
+        public TextView tvFinished;
+
+
+        public InProgressRacesViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
+            super(itemView);
+
+            ivPublic = itemView.findViewById(R.id.inprogress_races_item_ivPublic);
+            ivMenu = itemView.findViewById(R.id.ivMenu);
+
+            tvTag = itemView.findViewById(R.id.inprogress_races_item_tag);
+            tvCreator = itemView.findViewById(R.id.inprogress_races_item_creator);
+            tvStartMethod = itemView.findViewById(R.id.inprogress_races_item_startMethod);
+            tvMode = itemView.findViewById(R.id.inprogress_races_item_mode);
+            tvVehiclesNo = itemView.findViewById(R.id.inprogress_races_item_vehiclesNo);
+            tvLaps = itemView.findViewById(R.id.inprogress_races_item_laps);
+            tvInProgress = itemView.findViewById(R.id.tv_inprogress);
+            tvCanceled = itemView.findViewById(R.id.tv_canceled);
+            tvFinished = itemView.findViewById(R.id.tv_finished);
+
+            // Crete popup menu
+            ivMenu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    PopupMenu popup = new PopupMenu(mContext, ivMenu);
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+
+                            switch (item.getItemId()) {
+
+                                // Cancel race
+                                case R.id.item_cancel_race:
+                                    if (listener != null) {
+                                        int position = getAdapterPosition();
+                                        if (position != RecyclerView.NO_POSITION) {
+                                            listener.onCancel(position);
+                                        }
+                                    }
+                                    return true;
+
+                                // View vehicles
+                                case R.id.item_vehicles:
+                                    if (listener != null) {
+                                        int position = getAdapterPosition();
+                                        if (position != RecyclerView.NO_POSITION) {
+                                            listener.onViewVehicles(position);
+                                        }
+                                    }
+                                    return true;
+
+                            }
+
+                            return false;
+                        }
+                    });
+                    popup.inflate(R.menu.inprogress_races_menu);
+                    Util.enablePopupIcons(popup);
+                    popup.show();
+                }
+            });
+
+
+        }
+    }
+
+    // CONSTRUCTOR WITH AN ARRAY LIST OF inprogress races
+    public InProgressRacesAdapter(Context context, List<Race> raceList) {
+        mContext = context;
+        mRaceList = raceList;
+    }
+
+    @NonNull
+    @Override
+    public InProgressRacesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.inprogress_races_item, parent, false);
+        InProgressRacesViewHolder viewHolder = new InProgressRacesViewHolder(v, mItemListener);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InProgressRacesViewHolder holder, int position) {
+        Race race = mRaceList.get(position);
+
+        // Public
+        if (race.isPublic()) {
+            holder.ivPublic.setVisibility(View.VISIBLE);
+        }
+        else {
+            holder.ivPublic.setVisibility(View.INVISIBLE);
+        }
+
+        // Tag
+        holder.tvTag.setText(race.getTag());
+
+        // creator
+        holder.tvCreator.setText("(" + race.getCreator().getUsername() + ")");
+
+        // Start Method
+        holder.tvStartMethod.setText(mContext.getString(R.string.start_method) + ": " + race.getStartMethod().getDescription());
+
+        // Mode
+        holder.tvMode.setText(mContext.getString(R.string.activator) + ": " + race.getMode().getDescription());
+
+        // Number of vehicles
+        holder.tvVehiclesNo.setText(mContext.getString(R.string.vehicles) + ": " + String.valueOf(race.getTotalVehicles()));
+
+        // Laps
+        holder.tvLaps.setText(mContext.getString(R.string.laps) + ": " + String.valueOf(race.getLaps()));
+
+        // In progress vehicles
+        holder.tvInProgress.setText(String.valueOf(race.getRunningVehicles()));
+
+        // Canceled vehicles
+        holder.tvCanceled.setText(String.valueOf(race.getCanceledVehicles()));
+
+        // Finished vehicles
+        holder.tvFinished.setText(String.valueOf(race.getFinishedVehicles()));
+
+     }
+
+    @Override
+    public int getItemCount() {
+        return mRaceList.size();
+    }
+
+    public void setRaceList(List<Race> raceList) {
+        mRaceList = raceList;
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/gr/artibet/lapper/fragments/InProgressRacesFragment.java
+++ app/src/main/java/gr/artibet/lapper/fragments/InProgressRacesFragment.java
@@ -4,18 +4,42 @@ package gr.artibet.lapper.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import gr.artibet.lapper.R;
+import gr.artibet.lapper.adapters.InProgressRacesAdapter;
+import gr.artibet.lapper.api.RetrofitClient;
+import gr.artibet.lapper.models.Race;
+import gr.artibet.lapper.models.RaceResponse;
+import gr.artibet.lapper.models.RaceState;
+import gr.artibet.lapper.storage.SharedPrefManager;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InProgressRacesFragment extends Fragment {
 
+    private List<Race> mRaceList = new ArrayList<>();
+    private ProgressBar mProgressBar;
+    private TextView mTvMessage;
+
+    // Recycler view members
+    private RecyclerView mRecyclerView;
+    private InProgressRacesAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public InProgressRacesFragment() {
         // Required empty public constructor
@@ -26,7 +50,102 @@ public class InProgressRacesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_in_progress_races, container, false);
+        View v = inflater.inflate(R.layout.fragment_in_progress_races, container, false);
+
+        // Initialize views and layouts
+        mProgressBar = v.findViewById(R.id.progressBar);
+        mTvMessage = v.findViewById(R.id.tvMessage);
+        mRecyclerView = v.findViewById(R.id.recyclerView);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+
+        mAdapter = new InProgressRacesAdapter(getActivity(), mRaceList);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Set race item click listener
+        mAdapter.setOnItemClickListener(new InProgressRacesAdapter.OnItemClickListener() {
+            @Override
+            public void onCancel(int position) {
+                cancelRace(position);
+            }
+
+            @Override
+            public void onViewVehicles(int position) {
+                viewVehicles(position);
+            }
+
+        });
+
+
+        // Fetch data from API and return
+        fetchRaces();
+
+        // Return view
+        return v;
+    }
+
+    public void fetchRaces() {
+
+        // Show progress bar and hide message text
+        mProgressBar.setVisibility(View.VISIBLE);
+        mTvMessage.setVisibility(View.INVISIBLE);
+
+        Call<RaceResponse> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .getRaces(SharedPrefManager.getInstance(getActivity()).getToken(), RaceState.STATE_INPROGRESS);
+
+        call.enqueue(new Callback<RaceResponse>() {
+            @Override
+            public void onResponse(Call<RaceResponse> call, Response<RaceResponse> response) {
+
+                if (!response.isSuccessful()) {
+                    mTvMessage.setText(response.message());
+                    mTvMessage.setVisibility(View.VISIBLE);
+                    //Util.errorToast(getActivity(), response.message);
+                }
+                else {
+                    mRaceList = (response.body()).getResults();
+                    mAdapter.setRaceList(mRaceList);
+
+                    if (mRaceList.size() == 0) {
+                        mTvMessage.setText(getResources().getString(R.string.no_race_in_progress));
+                        mTvMessage.setVisibility(View.VISIBLE);
+                    }
+                    else {
+                        mTvMessage.setVisibility(View.INVISIBLE);
+                    }
+                }
+
+                // Hide progress bar
+                mProgressBar.setVisibility(View.INVISIBLE);
+
+            }
+
+            @Override
+            public void onFailure(Call<RaceResponse> call, Throwable t) {
+                mTvMessage.setText(t.getMessage());
+                //mTvMessage.setText(getString(R.string.unable_to_connect));
+                mTvMessage.setVisibility(View.VISIBLE);
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+
+    // Deactivate race
+    private void cancelRace(final int position) {
+
+        final Race race = mRaceList.get(position);
+
+    }
+
+
+    // View Race vehicles
+    private void viewVehicles(int position) {
+
     }
 
 }
+
